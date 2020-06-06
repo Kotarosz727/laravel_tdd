3f029a43c7acf22342c381b3b36b2623e5c9c53e
Kotaro Suzuki <kotarosz727@gmail.com>
2020-06-06 18:15:31 +0900

    add api test

--- code/sample_tdd/tests/Feature/ReportTest.php
+++ code/sample_tdd/tests/Feature/ReportTest.php
@@ -10,7 +10,7 @@ class ReportTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
         $this->artisan('db:seed', ['--class' => 'TestDataSeeder']);
@@ -29,7 +29,10 @@ class ReportTest extends TestCase
      */
     public function post_response_code_200()
     {
-        $response = $this->post('api/customers');
+        $customer = [
+            'name' => 'customer_name',
+        ];
+        $response = $this->postJson('api/customers', $customer);
         $response->assertStatus(200);
     }
 
@@ -112,5 +115,68 @@ class ReportTest extends TestCase
     {
         $response = $this->delete('api/reports/1');
         $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function get_json_api_customer()
+    {
+        $response = $this->get('api/customers');
+        $this->assertThat($response->content(), $this->isJson());
+    }
+
+    /**
+     * @test
+     */
+    public function get_correct_json_attribute()
+    {
+        $response = $this->get('api/customers');
+        $customers = $response->json();
+        $customers = $customers[0];
+        $this->assertSame(['id', 'name'], array_keys($customers));
+    }
+
+    /**
+     * @test
+     */
+    public function get_json_data_two_customers_data()
+    {
+        $response = $this->get('api/customers');
+        $response->assertJsonCount(2);
+    }
+
+    /**
+     * @test
+     */
+    public function post_customers_data_to_database()
+    {
+        $params = [
+            'name' => '顧客名',
+        ];
+        $this->postJson('api/customers', $params);
+        $this->assertDatabaseHas('customers', $params);
+    }
+
+    /**
+     * @test
+     */
+    public function POST_api_customers_にnameが含まれない場合422が返却される()
+    {
+        $params = [];
+        $response = $this->postJson('api/customers', $params);
+        $response->assertStatus(\Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /**
+     * @test
+     */
+    public function POST_api_customers_にnameが空の場合422が返却される()
+    {
+        $params = [
+            'name' => ''
+        ];
+        $response = $this->postJson('api/customers', $params);
+        $response->assertStatus(\Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 }
